8b68638b7d4f70a82b7cc35dd2076d9b897f6733
AliCoskunK <kulaklialicoskun@gmail.com>
2024-05-07 20:53:54 +0300

    Değişken Tanımlama

---
+++ app/src/main/java/com/example/kotlinbootcamp/degiskenler/Odev1.kt
@@ -0,0 +1,107 @@
+package com.example.kotlinbootcamp.degiskenler
+
+fun main() {
+
+    val ilce : String = "Kozan"
+    val kita : String="Avrupa"
+    val faks : Int= 23456789
+    val postakod : Long= 45687
+    val instaadres : String="devlife"
+    val calistigibolum : String ="Yazılım"
+    val urunmiktari : Int=100
+    val musterisoyadi : String="Engin"
+    val odememik : Int=450
+    val dogumtarihi: Int=1990
+    val borc : Double=100.235
+    val medenihal:String="Evli"
+    val videoyorum:String="Çok iyi"
+    val odemeSaati:Double=12.30
+    val eftmik:Double=100.455
+    val satilanmik:Int=1500
+    val telefonmodel:String="Galaxy S21"
+    val dergiadi:String="Kafa"
+    val basimtarih:String="12/10/1960"
+    val zammik:String ="%40"
+    val daireSayi:Int=85
+    val enlem:Double=40.55
+    val boylam:Double=30.40
+    val yemekadi:String="Hamburger"
+    val ürünfiyat:Int=100
+    val sirket:String="KODLAB"
+    val videoadi:String="Kotlin Geliştirici Olmak"
+    val muziksuresi:Double=12.12
+    val mekanpuani:Double=5.0
+    val dosyaadi:String="Klasör"
+    val resimformati:String="jpg"
+    val renk:String="Mavi"
+    val renkkodu:String="#ff4444"
+    val bilgisayamodeli:String="Mac"
+    val ekranboyutu:Int=15
+    val kullanimsuresi:Int=45
+    val basınc:Double=10.5
+    val etkinlikgunu: String= "Çarşamba"
+    val ödemegunu :String="Cuma"
+    val yolculukcıkıs :Double=12.01
+    val mahalle:String="Yenimahalle"
+    val otobushat:String="HasTurizm"
+    val kullanilandakika:Double=155.22
+    val kargotakipno:Long=45464654
+    val kuponsure:Double=5.96
+    val kuponkodu:Int=15466
+    val faturatarihi:Double=12.04
+
+    println("ilçe adı : $ilce")
+    println("Kıta ismi: $kita")
+    println("Faks numarası: $faks")
+    println("Posta kodu: $postakod")
+    println("İnstagram adresi :$instaadres")
+    println("Çalıştığı bölüm: $calistigibolum")
+    println("Ürün miktarı: $urunmiktari")
+    println("Müşterinin soyadı: $musterisoyadi")
+    println("Ödeme Miktarı: $odememik")
+    println("Doğum tarihi :$dogumtarihi")
+    println("Borç miktarı: $borc")
+    println("Medeni durumu: $medenihal")
+    println("Videoya yapılan yorum: $videoyorum")
+    println("Ödeme saati: $odemeSaati")
+    println("Eft miktarı: $eftmik")
+    println("Satılan miktar: $satilanmik")
+    println("Telefonun modeli: $telefonmodel")
+    println("Dergi adı: $dergiadi")
+    println("Basım tarihi: $basimtarih")
+    println("Zam miktarı: $zammik")
+    println("Daire sayısı: $daireSayi")
+    println("Enlem: $enlem")
+    println("Boylam:$boylam")
+    println("Yemek ismi :$yemekadi")
+    println("Ürünün fiyatı :$ürünfiyat")
+    println("Şirket ismi :$sirket")
+    println("Video adı : $videoadi")
+    println("Müzik süresi :$muziksuresi")
+    println("Mekan puanı: $mekanpuani")
+    println("Dosya adı: $dosyaadi")
+    println("Resim formati: $resimformati")
+    println("Renk ismi: $renk")
+    println("Renk kodu :$renkkodu")
+    println("Bilgisayar modeli: $bilgisayamodeli")
+    println("Ekran boyutu: $ekranboyutu")
+    println("Kullanım süresi :$kullanimsuresi")
+    println("Basınç :$basınc")
+    println("Etkinliğin günü :$etkinlikgunu")
+    println("Ödeme günü :$ödemegunu")
+    println("Yolculuk Çıkış Saati: $yolculukcıkıs")
+    println("Mahalle ismi: $mahalle")
+    println("Otobüs hattı :$otobushat")
+    println("Kullanılan dakika :$kullanilandakika")
+    println("Kargo takip no: $kargotakipno")
+    println("Kupon süresi: $kuponsure")
+    println("Kupon kodu: $kuponkodu")
+    println("Fatura tarihi: $faturatarihi")
+
+
+
+
+
+
+
+}
